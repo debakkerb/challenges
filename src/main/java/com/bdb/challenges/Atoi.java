d2516b364962271e1402bf12002da987331708e6
Bjorn De Bakker <bbakker@google.com>
2020-12-21 12:50:21 +0000

    Adding tests

--- src/main/java/com/bdb/challenges/Atoi.java
+++ src/main/java/com/bdb/challenges/Atoi.java
@@ -1,7 +1,18 @@
 package com.bdb.challenges;
 
 class Atoi {
+    private static final int WHITESPACE = 32;
+    private static final int ZERO = 48;
+    private static final int NINE = 57;
+
     public int myAtoi(String s) {
-        return Integer.MAX_VALUE;
+        StringBuilder input = new StringBuilder();
+        int output = 0;
+
+        for (char c : s.toCharArray()) {
+
+        }
+
+        return output;
     }
 }
